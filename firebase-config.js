5ece81e56162aee766ca9402486055493facb2f0
johnateofilo-ops <john.a.teofilo@gmail.com>
2026-09-13 14:46:56 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration for this duplicate deployment.
+// Copied from original and intended to be used for the Firebase-connected copy.
+window.firebaseConfig = {
+  apiKey: "AIzaSyCl2_z2ss58bgzQnJBxXHFDo02jeV-dUGM",
+  authDomain: "teofi-d1276.firebaseapp.com",
+  databaseURL: "https://teofi-d1276-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "teofi-d1276",
+  storageBucket: "teofi-d1276.firebasedatabase.app",
+  messagingSenderId: "56822276463",
+  appId: "1:56822276463:web:be058251dd1a1bd85f489a",
+  measurementId: "G-CNJ7GKYDNX"
+};
